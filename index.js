449340e4fe7eb71771fd9766e68a908c22357772
LoveIsGrief <loveisgrief@tuta.io>
2021-01-28 16:18:18 +0100

    Init, that starts...
    It immediately nopes out after starting a torrent
    
    ```
    [warn] epoll_create: Too many open files
    [err] evsignal_init: socketpair: Too many open files
    ```

---
+++ index.js
@@ -0,0 +1,18 @@
+const {program} = require('commander');
+const {main} = require("./lib/main");
+
+program
+    .version('0.0.1')
+    .option("-c, --count <number>", "Number of videos to reseed", 10)
+    .option("-d, --downloadPath <string>", "Download dir of all videos", "/tmp/peertube-reseed/")
+    .arguments("<targetServer>")
+    .description(
+        "peertube-reseed", {
+          targetServer: "Which server to help reseed trending videos"
+        }
+    )
+    .action(async(targetServer, options, command) => {
+      await main({targetServer, ...options})
+    });
+
+program.parse();
